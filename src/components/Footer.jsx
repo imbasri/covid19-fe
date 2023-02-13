833abd27f581fe774bc96cfdfdcef81fffce7ff7
imbasri <imbasri.dev@gmail.com>
2023-02-13 14:37:31 +0700

    update responsive Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,15 +9,15 @@ import {
 import { Link } from "react-router-dom";
 function Footer() {
    return (
-      <footer>
-         <div>
+      <footer className="container mx-auto w-full text-center">
+         <div className="flex flex-col items-center md:flex-row md:justify-between">
             <div className="flex items-center justify-center">
-               <img src={icon} alt="icon_covid19" className="mr-2" />
+               <img src={icon} alt="icon_covid19" className="mr-2 md:w-10" />
                <span className="block font-hat text-base font-bold text-secondary">
                   Covid-19
                </span>
             </div>
-            <div className="my-4 flex">
+            <div className="my-4 w-1/2  p-10 md:ml-10 md:p-2">
                <ul className="flex w-full flex-wrap items-center justify-center  gap-4 font-hat text-sm font-bold text-secondary">
                   <li>
                      <Link to={"#"}>Overview</Link>
@@ -41,7 +41,7 @@ function Footer() {
                            <Facebook
                               color="default"
                               size={30}
-                              className=" w-auto cursor-pointer"
+                              className=" w-auto cursor-pointer md:w-5"
                            />
                         </Link>
                      }
@@ -52,7 +52,7 @@ function Footer() {
                            <Youtube
                               color="default"
                               size={30}
-                              className=" w-auto cursor-pointer"
+                              className=" w-auto cursor-pointer md:w-5"
                            />
                         </Link>
                      }
@@ -63,7 +63,7 @@ function Footer() {
                            <Twitter
                               color="default"
                               size={30}
-                              className=" w-auto cursor-pointer"
+                              className=" w-auto cursor-pointer md:w-5"
                            />
                         </Link>
                      }
@@ -74,17 +74,20 @@ function Footer() {
                            <Vimeo
                               color="default"
                               size={30}
-                              className=" w-auto cursor-pointer"
+                              className=" w-auto cursor-pointer md:w-5"
                            />
                         </Link>
                      }
                   </li>
                </ul>
             </div>
-            <span className="flex justify-center text-center text-sm font-bold text-secondary">
+            <span className="my-5 flex justify-center text-center text-sm font-bold text-secondary md:hidden">
                2023 @ All rights coding☕ by imbasri
             </span>
          </div>
+         <span className="my-5 hidden justify-center text-center text-sm font-bold text-secondary md:block">
+            2023 @ All rights coding☕ by imbasri
+         </span>
       </footer>
    );
 }
